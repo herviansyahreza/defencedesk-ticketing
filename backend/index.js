621f3c97ed1e38f7421ff638af97d194b07db840
herviansyahreza <82496908+herviansyahreza@users.noreply.github.com>
2024-07-15 14:13:21 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 5001;
 const db = require('./db.config/db.config');
 
-const cors = require('cors');
+//const cors = require('cors');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const userRouter = require('./router/router');
@@ -15,10 +15,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser()); // Hapus opsi maxAge di sini
 
 // Konfigurasi CORS
-app.use(cors({
-    origin: '*', // Atur origin sesuai dengan domain frontend Anda
-    credentials: true,
-}));
+//app.use(cors({
+//    origin: '*', // Atur origin sesuai dengan domain frontend Anda
+//    credentials: true,
+//}));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://172.16.1.251:5000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 // Route
 app.use('/', userRouter);
