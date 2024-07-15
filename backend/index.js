2ec308d6d8d9af2ca70d7a51ebdd738df2fb0ceb
herviansyahreza <82496908+herviansyahreza@users.noreply.github.com>
2024-07-15 14:15:08 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -20,14 +20,6 @@ app.use(cookieParser()); // Hapus opsi maxAge di sini
 //    credentials: true,
 //}));
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://172.16.1.251:5000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
-
 // Route
 app.use('/', userRouter);
 
