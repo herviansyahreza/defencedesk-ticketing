2138e38fd24c01026d666d3dfb9a6479b3e78760
herviansyahreza <82496908+herviansyahreza@users.noreply.github.com>
2024-07-15 13:50:12 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(cookieParser()); // Hapus opsi maxAge di sini
 
 // Konfigurasi CORS
 app.use(cors({
-    origin: 'http://172.16.1.251:5000', // Atur origin sesuai dengan domain frontend Anda
+    origin: '*', // Atur origin sesuai dengan domain frontend Anda
     credentials: true,
 }));
 
